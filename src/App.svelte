8ba1d9b118400d7272bad66a488d68100a7191a2
Adam Siekierski <a@siekierski.ml>
2020-08-13 19:44:26 +0200

    Simple prompt

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-
+  let value = '';
 </script>
 
-<style>
-  .svelte {
-    color: #ff3e00;
-  }
-
+<style type="text/scss">
   .wrapper {
     height: 100vh;
     display: flex;
@@ -14,11 +10,24 @@
     justify-content: center;
     align-items: center;
   }
+
+  .input {
+    background: none;
+    color: white;
+    border: 1px solid #222;
+    border-radius: 5px;
+    padding: 10px;
+    outline: none;
+    transition: box-shadow 0.5s ease;
+    font-family: inherit;
+
+    &:focus {
+      box-shadow: 0px 0px 0px 3px orangered;
+    }
+  }
 </style>
 
 <main class="wrapper">
-  <h1>
-    hello,
-    <span class="svelte">svelte!</span>
-  </h1>
+  <h1>what can i buy for...</h1>
+  <input type="text" {value} class="input" placeholder="monni" />
 </main>
