e7e8703893b12ceb664bd0c8321c47f35e447deb
ejer <ejer.priv@wp.pl>
2022-10-30 23:56:09 +0100

    Display price ranges below + pretty styling

--- src/App.svelte
+++ src/App.svelte
@@ -8,28 +8,87 @@
   }
 
   let money: number;
-  let result: ResultItem;
+  let result: ResultItem[];
 
   function showResult() {
-    result = values.reduce((previousValue, currentValue) =>
-      money >= currentValue.from ? currentValue : previousValue,
-    );
+    result = values.filter(item => item.from <= money).reverse();
   }
 </script>
 
 <style type="text/scss">
+  .container {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
   .wrapper {
-    height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
     text-align: center;
+    flex: 1;
+  }
+
+  .result {
+    max-width: 50ch;
+    flex: 1;
+  }
+
+  .category {
+    .price {
+      font-size: 1.25rem;
+      position: relative;
+      text-align: left;
+      color: #ffffff30;
+      font-weight: 500;
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      margin-bottom: 0;
+      margin-top: 1rem;
+
+      .line {
+        flex: 1;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        border-top: 1px solid #ffffff30;
+        border-bottom: 1px solid #ffffff30;
+        z-index: -1;
+      }
+    }
+
+    ul {
+      padding-left: 3rem;
+      padding-right: 1.5rem;
+
+      li {
+        text-align: left;
+        padding-left: 1rem;
+        color: #ffffff70;
+      }
+    }
+
+    &:first-of-type {
+      .price {
+        color: #ffffff;
+
+        .line {
+          border-color: #ffffff;
+        }
+      }
+
+      li {
+        color: #ffffff;
+      }
+    }
   }
 
   .footer {
     text-align: center;
-    padding: 2px;
+    padding: 0.5rem;
     font-size: 0.8em;
 
     a {
@@ -44,21 +103,35 @@
   }
 </style>
 
-<main class="wrapper">
-  <div>
-    <h1>what can i buy for...</h1>
-    <Money bind:value={money} on:input={showResult} />
-  </div>
-  {#if result}
-    <div class="result">
-      {#each result.things.sort() as thing}
-        <p>{thing}</p>
-      {/each}
+<div class="container">
+  <main class="wrapper">
+    <div>
+      <h1>what can i buy for...</h1>
+      <Money bind:value={money} on:input={showResult} />
     </div>
-  {/if}
-  <div />
-</main>
-<footer class="footer">
-  made by
-  <a href="https://siekierski.ml">adam siekierski</a>
-</footer>
+    {#if result}
+      <div class="result">
+        {#each result as price}
+          <div class="category">
+            <h2 class="price">
+              <span>${price.from}</span>
+              <span class="line" />
+            </h2>
+            <ul>
+              {#each price.things.sort() as thing}
+                <li>
+                  <p>{thing}</p>
+                </li>
+              {/each}
+            </ul>
+          </div>
+        {/each}
+      </div>
+    {/if}
+    <div />
+  </main>
+  <footer class="footer">
+    made by
+    <a href="https://siekierski.ml">adam siekierski</a>
+  </footer>
+</div>
